422ca59d2462f11de2ef9ab42cf337965851d13f
Pavel Dvorkin <dpavel20@gmail.com>
2014-07-21 17:21:44 -0400

    Update ROT-13.py

--- ROT-13.py
+++ ROT-13.py
@@ -1,5 +1,9 @@
+"""Method 1 - String ROT13
+Encrypt user strings in ROT 13 encryption. ROT-13 is a simple substitution cypher. It stands for "ROTate by 13 places."
+Thus the cypher replaces any letter (a-z or A-Z) with the one that appears 13 sequential places behind it. 
 
-
+For letters in the last half of the alphabet, their ROT-13 equivalents loop back around to the front of the alphabet. 
+Any non-alphabetical characters simply pass through."""
 
 
 def string_rot13(str):
